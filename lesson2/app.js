901221c0876e0f8877248dd139e20467f8ef1d74
SyimykMitalipov <syimyk310703@gmail.com>
2022-11-08 17:45:47 +0600

    less 2

---
+++ lesson2/app.js
@@ -0,0 +1,77 @@
+// console.log('2')
+// // console.log('1')
+//
+//
+// // const timeOut = setTimeout(() => {
+// //     console.log('timeout')
+// // },0)
+// //
+// // clearTimeout(timeOut)
+//
+// // let num = 0;
+// // let inter = setInterval(() => {
+// //     num++
+// //     console.log(num)
+// },100)
+
+
+
+
+// const box = document.querySelector('.box')
+
+
+// let leftPos = 0;
+// let topPos = 0;
+// const moveBlock = () => {
+//     if (leftPos <= 380){
+//         leftPos += 20
+//         box.style.left = `${leftPos}px`
+//         setTimeout(moveBlock,100)
+//     }else if(leftPos >=380 && topPos <= 280){
+//         topPos += 20
+//         box.style.top = `${topPos}px`
+//         setTimeout(moveBlock,100)
+//     }
+// }
+//
+// moveBlock()
+//
+
+
+
+// function getSum(n) {
+//     let ind = 6
+//    return function getNSum(num){
+//        let i = num*n+ind
+//        return function getN2Sum(num2){
+//            let i2 = num2 + num + n
+//            console.log(i2)
+//        }
+//    }
+// }
+// const resOfFc = getSum(2)
+// const resofFc2 = resOfFc(4)
+// resofFc2(3)
+//
+
+
+// console.log('log')
+// setTimeout(() => {
+//     console.log(timeout)
+// },2000)
+
+
+// function isPalindrome(x) {
+//     return x === x.split('').reverse().join('')
+// }
+//
+// console.log(isPalindrome('sas'))
+
+
+
+
+
+
+
+
+
